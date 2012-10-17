a00f0adf22e92ee53b921a625b6f6d432582e2ca
MikrolaX <seb@mikrolax.me>
2012-10-17 12:41:59 +0200

    wav FORMAT EXT handling (skip 'fact' chunk instead of abortiing

--- ebu_r128/examples/wave/wave.c
+++ ebu_r128/examples/wave/wave.c
@@ -1,6 +1,6 @@
 /*
  *  Loudness analyze library
- *  implementing EBU R.128  
+ *  implementing EBU R.128
  *
  *  Small and dirty wave library
  *
@@ -18,72 +18,77 @@ int parse_wavefile(FILE *f, s_wave_header *wh) {
 
     size_t read=0;
     unsigned int riff=0x0;
-    
-    read=fread(&riff, 1, sizeof(riff), f); 
+
+    read=fread(&riff, 1, sizeof(riff), f);
 
     if (read != 4)
-	return 0;
+    return 0;
 
     if (riff != 0x46464952)  // RIFF
-	return 0;
+    return 0;
 
 
 
     unsigned int chunksize=0;
 
-    read=fread(&chunksize, 1, sizeof(chunksize), f); 
+    read=fread(&chunksize, 1, sizeof(chunksize), f);
 
     if (read != 4)
-	return 0;
+    return 0;
 
     wh->total_size = chunksize;
 
 
 
     unsigned int wave=0x0;
-    
-    read=fread(&wave, 1, sizeof(wave), f); 
+
+    read=fread(&wave, 1, sizeof(wave), f);
 
     if (read != 4)
-	return 0;
+    return 0;
 
     if (wave != 0x45564157)  // WAVE
-	return 0;
+    return 0;
 
     wh->headers_valid = 1;
 
 
     while (!feof(f)) {
 
-	unsigned int chunk_id=0;
-	unsigned int chunk_len=0;
+    unsigned int chunk_id=0;
+    unsigned int chunk_len=0;
 
-	read=fread(&chunk_id, 1, sizeof(chunk_id), f); 
+    read=fread(&chunk_id, 1, sizeof(chunk_id), f);
 
-	if (read != 4)
-	    return 0;
+    if (read != 4)
+        return 0;
 
-	read=fread(&chunk_len, 1, sizeof(chunk_len), f); 
+    read=fread(&chunk_len, 1, sizeof(chunk_len), f);
 
-	if (read != 4)
-	    return 0;
+    if (read != 4)
+        return 0;
 
-	switch (chunk_id) {
+    switch (chunk_id) {
+        fprintf(stderr, "chunk 0x%04x\n", chunk_id);
 
-	    case 0x20746d66:  // fmt
-		wave_parse_fmt(f, wh, chunk_len);		
-	    break;
-	
-	    case 0x61746164: // data
-		wh->audio_size=chunk_len;
-		return 1;
-    
+        case 0x20746d66:  // fmt
+        wave_parse_fmt(f, wh, chunk_len);
+        break;
 
-	    default:
-		fprintf(stderr, "Unknown chunk type 0x%04x\n", chunk_id);
-		fseek(f, chunk_len, SEEK_CUR);
-		return 0;
-	}
+        case 0x61746164: // data
+        wh->audio_size=chunk_len;
+        return 1;
+
+        case 0x74636166: // fact
+        //fprintf(stderr, "Wave EXT detected skip chunk! \n");
+        fseek(f, chunk_len, SEEK_CUR);
+        break;
+
+        default:
+        fprintf(stderr, "Unknown chunk type 0x%04x\n", chunk_id);
+        fseek(f, chunk_len, SEEK_CUR);
+        return 0;
+    }
     }
 
     return 1;
@@ -98,11 +103,11 @@ void wave_parse_fmt(FILE *f, s_wave_header *wh, unsigned int len) {
     size_t read = fread(data, 1, len, f);
 
     if (read == len) {
-	wh->compression = data[1] << 8 | data[0];
-	wh->channels =    data[3] << 8 | data[2];
-	wh->sample_rate =  data[7] << 24 | data[6] << 16 |data[5] << 8 | data[4];
-	wh->resolution = data[15] << 8 | data[14];
-	wh->block_align =  data[13] << 8 | data[12];
+    wh->compression = data[1] << 8 | data[0];
+    wh->channels =    data[3] << 8 | data[2];
+    wh->sample_rate =  data[7] << 24 | data[6] << 16 |data[5] << 8 | data[4];
+    wh->resolution = data[15] << 8 | data[14];
+    wh->block_align =  data[13] << 8 | data[12];
     }
 
 }
